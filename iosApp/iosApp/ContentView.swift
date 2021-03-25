13ae166acf041fbc1eda94c0f65e6f389add15f0
yaqiao <yaqiao@Ctrip.com>
2021-03-25 16:50:30 +0800

    iOS 端首页列表开发完成

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,11 +4,13 @@ import shared
 
 struct ContentView: View {
     
+    @ObservedObject var fetcher = NewsSummaryFetcher()
+    
     var body: some View {
-        NetworkRequest.init().getNewsSummaryList { (list: [NewsSummary]?, error: Error?) in
-            list?.forEach({ (summary: NewsSummary) in
-                NewsSummaryRow(newsSummary: <#T##NewsSummary#>)
-            })
+        NavigationView {
+            List(fetcher.newsSummaryList) { summary in
+                NewsSummaryRow(newsSummary: summary)
+            }.navigationBarTitle("Kotlin 新闻",displayMode: .inline)
         }
     }
 }
@@ -19,34 +21,58 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+public class NewsSummaryFetcher: ObservableObject {
+    
+    @Published var newsSummaryList = [NewsSummary]()
+    
+    init() {
+        load()
+    }
+    
+    func load() {
+        NetworkRequest.init().getNewsSummaryList { (list: [NewsSummary]?, error: Error?) in
+            if (list != nil) {
+                self.newsSummaryList = list!
+            }
+        }
+    }
+}
+
 
 struct NewsSummaryRow: View {
     
     var newsSummary: NewsSummary
     
+    @State private var remoteImage : UIImage? = nil
+    let placeholderOne: UIImage = UIImage()
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(newsSummary.title).font(.title)
-            Image(newsSummary.imageUrl)
+            Image(uiImage: self.remoteImage ?? placeholderOne)
+                        .onAppear(perform: fetchRemoteImage)
             Text(newsSummary.summary).font(.subheadline)
             Text(newsSummary.date).font(.subheadline)
         }
-        
+    }
+    
+    func fetchRemoteImage() {
+        guard let url = URL(string: newsSummary.imageUrl) else { return } //初始化一个字符串常量，作为网络图片的地址
+        URLSession.shared.dataTask(with: url){ (data, response, error) in //执行URLSession单例对象的数据任务方法，以下载指定的图片
+            if let image = UIImage(data: data!){
+                self.remoteImage = image //当图片下载成功之后，将下载后的数据转换为图像，并存储在remoteImage属性中
+            } else {
+                print(error ?? "") //如果图片下载失败之后，则在控制台输出错误信息
+            }
+        }.resume() //通过执行resume方法，开始下载指定路径的网络图片
     }
 }
 
 extension NewsSummary: Identifiable {
     public var id: String {
         get {
-            self.id
+            self.title
         }
     }
 }
 
-func getNetwork() -> Void {
-    NetworkRequest.init().getNewsSummaryList { (list: [NewsSummary]?, error: Error?) in
-        list?.forEach({ (summary: NewsSummary) in
-            print(summary.title)
-        })
-    }
-}
--- shared/src/commonMain/kotlin/com/qiaoyuang/kmmnews/shared/NetworkRequest.kt
+++ shared/src/commonMain/kotlin/com/qiaoyuang/kmmnews/shared/NetworkRequest.kt
@@ -3,7 +3,6 @@ package com.qiaoyuang.kmmnews.shared
 import com.qiaoyuang.kmmnews.shared.data.NewsContent
 import com.qiaoyuang.kmmnews.shared.data.NewsSummary
 import io.ktor.client.request.*
-import io.ktor.client.utils.*
 
 /**
  * 网络请求管理器
@@ -11,7 +10,7 @@ import io.ktor.client.utils.*
 
 object NetworkRequest {
 
-    private const val URL_GET_NEWS_LIST = "http://192.168.1.3:8080/news"
+    private const val URL_GET_NEWS_LIST = "http://localhost:8080/news"
 
     suspend fun getNewsSummaryList(): List<NewsSummary>? = try {
         CLIENT.get(URL_GET_NEWS_LIST)
@@ -20,7 +19,7 @@ object NetworkRequest {
         null
     }
 
-    private const val URL_GET_NEWS_CONTENT = "http://192.168.1.3:8080/news"
+    private const val URL_GET_NEWS_CONTENT = "http://localhost:8080/news"
 
     suspend fun getNewsContent(newsSummary: NewsSummary): NewsContent? = try {
         CLIENT.post(URL_GET_NEWS_CONTENT) {
